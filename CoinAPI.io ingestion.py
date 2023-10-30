a16b67bbe0913c6f44227b322d053cb1e97a7e86
johnmathews <johnmathews>
2023-10-30 16:57:15 +0000

    add CoinAPI.io ingestion notebook

---
+++ CoinAPI.io ingestion.py
@@ -0,0 +1,29 @@
+# Databricks notebook source
+import requests
+import json
+from pyspark.sql import SparkSession
+
+# Initialize Spark session
+spark = SparkSession.builder.getOrCreate()
+
+# API Key (Stored as a Databricks secret for security)
+api_key = spark.conf.get("spark.databricks.secrets.my_scope.my_api_key")
+
+# API Endpoint
+url = "https://rest.coinapi.io/v1/ohlcv/BTC/USD/history?period_id=1DAY&time_start=2021-01-01T00:00:00"
+
+# API Headers
+headers = {
+  'X-CoinAPI-Key': api_key
+}
+
+# Make the API call
+response = requests.get(url, headers=headers)
+data = json.loads(response.text)
+
+# Convert to PySpark DataFrame
+spark_df = spark.createDataFrame(data)
+
+# Show DataFrame
+spark_df.show()
+
