7fb8bf2da90f3e77dba2bcc4735d7988df4a53a1
John Mathews <mthwsjc@gmail.com>
2023-10-31 14:44:10 +0100

    add api key retrieval to notebook

--- CoinAPI-ingestion.py
+++ CoinAPI-ingestion.py
@@ -7,7 +7,8 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.getOrCreate()
 
 # API Key (Stored as a Databricks secret for security)
-api_key = spark.conf.get("spark.databricks.secrets.my_scope.my_api_key")
+coinapi_api_key = spark.conf.get("spark.databricks.secrets.general.coinapi-api-key")
+coinmarketcap_api_key = spark.conf.get("spark.databricks.secrets.general.coinmarketcap-api-key")
 
 # API Endpoint
 url = "https://rest.coinapi.io/v1/ohlcv/BTC/USD/history?period_id=1DAY&time_start=2021-01-01T00:00:00"
